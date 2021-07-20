fd2401c78b4fae9ce303a1b5b4b2fa33797d04b8
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-07-20 06:31:14 -0700

    Initial commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + "/date.js");
 
 const app = express();
 const port = 3000;
@@ -13,14 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get('/', function (req, res) {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-    let day = today.toLocaleDateString("en-US", options);
+let day = date();
 
 res.render("list", { listTitle: day, newListItems: items });
 
