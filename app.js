b41722cbafc8033c361166f70619ea80d7bcd647
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-07-20 00:28:17 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.get('/', function (req, res) {
+  var today = new Date();
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+    var day = today.toLocaleDateString("en-US", options);
+
+res.render("list", { kindOfDay: day });
+
+});
+
+app.listen(port, function () {
+  console.log(`Server started on port ${port}`);
+});
